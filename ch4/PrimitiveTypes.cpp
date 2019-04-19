0ae0c54d4c3345746dfd1c3ab6eee33334ac53d7
Joe Cummings <jrcummings27@gmail.com>
2019-04-19 17:33:22 -0500

    first draft of primitive types

--- ch4/PrimitiveTypes.cpp
+++ ch4/PrimitiveTypes.cpp
@@ -14,6 +14,20 @@ int OneBitsInNum(int x){
 	return numOnes;
 }
 
+void OneBitsInNumTests(){
+	// Test 1
+	int result1 = OneBitsInNum(0);
+	if (result1 != 0){ printf("\x1b[31m" "OneBitsInNum Test 1 Failed" "\n"); }
+
+	// Test 2
+	int result2 = OneBitsInNum(2);
+	if (result2 != 1) { printf("\x1b[31m" "OneBitsInNum Test 2 Failed" "\n"); }
+
+	// Test 3
+	int result3 = OneBitsInNum(15);
+	if (result3 != 4) { printf("\x1b[31m" "OneBitsInNum Test 3 Failed" "\n"); }
+}
+
 // Isolate lowest bit, remove it, repeat until zero
 int OneBitsInNumFast(int x){
 	int numOnes = 0;
@@ -25,24 +39,18 @@ int OneBitsInNumFast(int x){
 	return numOnes;
 }
 
-void OneBitsInNumTests(){
+void OneBitsInNumFastTests(){
 	// Test 1
-	int result1 = OneBitsInNum(0);
-	int result12 = OneBitsInNumFast(0);
-	if (result1 != 0){ throw "OneBitsInNum Test 1 Failed"; }
-	if (result12 != 0){ throw "OneBitsInNumFast Test 1 Failed"; }
+	int result1 = OneBitsInNumFast(0);
+	if (result1 != 0){ printf("\x1b[31m" "OneBitsInNumFast Test 1 Failed" "\n"); }
 
 	// Test 2
-	int result2 = OneBitsInNum(2);
-	int result22 = OneBitsInNumFast(2);
-	if (result2 != 1) { throw "OneBitsInNum Test 2 Failed"; }
-	if (result22 != 1) { throw "OneBitsInNumFast Test 2 Failed"; }
+	int result2 = OneBitsInNumFast(2);
+	if (result2 != 1) { printf("\x1b[31m" "OneBitsInNumFast Test 2 Failed" "\n"); }
 
 	// Test 3
 	int result3 = OneBitsInNum(15);
-	int result32 = OneBitsInNum(15);
-	if (result3 != 4) { throw "OneBitsInNum Test 3 Failed"; }
-	if (result32 != 4) {throw "OneBitsInNumFast Test 3 Failed"; }
+	if (result3 != 4) { printf("\x1b[31m" "OneBitsInNumFast Test 3 Failed" "\n"); }
 }
 
 int PropogateRightMostBit(int x){
@@ -53,31 +61,59 @@ int PropogateRightMostBit(int x){
 void PropogateRightMostBitTests(){
 	// Test 1
 	int result1 = PropogateRightMostBit(80);
-	if (result1 != 95) { throw "PropogateRightMostBit Test 1 Failed"; }
+	if (result1 != 95) { printf("\x1b[31m" "PropogateRightMostBit Test 1 Failed" "\n"); }
 	
 	// Test 2
 	int result2 = PropogateRightMostBit(0);
-	if (result2 != 0) { throw "PropogateRightMostBit Test 2 Failed"; }
+	if (result2 != 0) { printf("\x1b[31m" "PropogateRightMostBit Test 2 Failed" "\n"); }
 }
 
 int Modulo(int x, int y){
-	return 0;
+	if (x < y){
+		return x;
+	}
+	int z = Modulo(x, y << 1);
+	if (z >= y){
+		z -= y;
+	}
+	return z;
 }
 
 void ModuloTests(){
 	// Test 1
-	int result1 = Modulo(1, 1);
-	if (result1 != 0) { throw "Modulo Test 1 Failed"; }
+	int result1 = Modulo(3, 3);
+	if (result1 != 0) { printf("\x1b[31m" "Modulo Test 1 Failed" "\n"); }
 	
 	// Test 2
 	int result2 = Modulo(77, 64);
 	if (result2 != 13) { printf("\x1b[31m" "Modulo Test 2 Failed" "\n"); }
+
+	// Test 3
+	int result3 = Modulo(10, 3);
+	if (result3 != 1) { printf("\x1b[31m" "Modulo Test 3 Failed" "\n"); }
+}
+
+bool PowerOfTwoHa(int x) {
+	if (Modulo(x, 2) == 0) return true;
+	return false;
+}
+
+void PowerOfTwoHaTests(){
+	// Test 1
+	bool result = PowerOfTwoHa(4);
+	if (!result) { printf("\x1b[31m" "PowerOfTwoHa Test 1 Failed" "\n"); }
+
+	// Test 2
+	bool result2 = PowerOfTwoHa(5);
+	if (result2) { printf("\x1b[31m" "PowerOfTwoHa Test 2 Failed" "\n"); }
 }
 
 int main(){
 	OneBitsInNumTests();
+	OneBitsInNumFastTests();
 	PropogateRightMostBitTests();
 	ModuloTests();
+	PowerOfTwoHaTests();
 
 	return 1;
 }
