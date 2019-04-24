e4d36b5f4c1a06cc7eca3579fcb4e0ddaa8ffae5
Joe Cummings <jrcummings27@gmail.com>
2019-04-24 18:16:13 -0500

    arrays done

--- ch4/Arrays.cpp
+++ ch4/Arrays.cpp
@@ -3,36 +3,34 @@
 
 using namespace std;
 
-int Partition(vector<int> arr, int low, int high){
-    int pivot = arr[high];
-
-    int i = low - 1;
-
-    for (int j = low; j <= high- 1; j++)
-    {
-        if (arr[j] <= pivot)
-        {
-            i++;
-            swap(arr[i],arr[j]);
-        }
-    }
-    swap(arr[i + 1], arr[high]);
-    return i + 1;
-}
-
 vector<int> Quicksort(vector<int> arr, int low, int high){
     if (low < high){
-        int pi = Partition(arr, low, high);
 
-        Quicksort(arr, low, pi - 1);
-        Quicksort(arr, pi + 1, high);
+        int pivot = arr[high];
+
+        int i = low - 1;
+
+        for (int j = low; j <= high- 1; j++)
+        {
+            if (arr[j] <= pivot)
+            {
+                i++;
+                swap(arr[i],arr[j]);
+            }
+        }
+        swap(arr[i + 1], arr[high]);
+
+        pivot = i + 1;
+
+        return Quicksort(arr, low, pivot - 1);
+        return Quicksort(arr, pivot + 1, high);
     }
     return arr;
 }
 
 vector<int> OptimizedQuicksort(vector<int> unsorted){ 
 
-    return Quicksort(unsorted, 0, unsorted.size());
+    return Quicksort(unsorted, 0, unsorted.size()-1);
 }
 
 void OptimizedQuicksortTests(){
@@ -42,7 +40,7 @@ void OptimizedQuicksortTests(){
     if (result1 != vector<int>{1,2,3,5,7,9}){ printf("\x1b[31m" "OptimizedQuicksort Test One Failed" "\n"); }
 
     // Test 2
-    vector<int> test2 = { 0, 1 };
+    vector<int> test2 = { 1, 0 };
     vector<int> result2 = OptimizedQuicksort(test2);
     if (result2 != vector<int>{ 0,1 }){ printf("\x1b[31m" "OptimizedQuicksort Test Two Failed" "\n"); }
 }
